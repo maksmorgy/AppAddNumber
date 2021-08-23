6294ad3d5d1b36fdb6e4d0f290ea64a02989be22
maksmorgy <maksmorgy.99@gmail.com>
2021-08-23 20:31:08 +0300

    addFiles

--- Add1/String.swift
+++ Add1/String.swift
@@ -1,8 +1,26 @@
-//
-//  String.swift
-//  Add1
-//
-//  Created by Максим Моргун on 10.08.2021.
-//
 
 import Foundation
+
+extension String {
+    
+    static func randomNumber(length: Int) -> String {
+        
+        var result = ""
+        
+        for _ in 0..<length {
+            let digit = Int.random(in: 0...9)
+            result += "\(digit)"
+        }
+        return result
+    }
+    
+    func integer(at n: Int) -> Int {
+        let index = self.index(self.startIndex, offsetBy: n)
+        
+        return self[index].wholeNumberValue ?? 0
+        
+    }
+    
+    
+    
+}
